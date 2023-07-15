b490452629b47a780d579a5d2ff71d94a96302a7
İhsan Eren Delibaş <hsan Eren Deliba@DESKTOP-RPEDS99>
2023-07-15 11:16:00 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata;
+
+namespace ymyp_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Eren";
+            string name2 = name;
+            name2 = "İhsan";
+            Console.WriteLine(name);
+            Console.WriteLine(name2);
+            User user = new();
+            User user2 = user;
+            user2.Name = "Emirhan";
+            Console.WriteLine(user.Name);
+        }
+    }
+}
+class User
+{
+    public string Name = "Delibas";
+}
